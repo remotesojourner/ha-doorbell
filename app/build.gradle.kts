10fd401cad2c420d7d5647927ef9a9e38167dbe8
remotesojourner <206970704+remotesojourner@users.noreply.github.com>
2026-09-17 00:20:20 +0100

    Fix broken tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,16 @@ android {
         unitTests {
             isIncludeAndroidResources = true
             isReturnDefaultValues = true
+            all { test ->
+                test.jvmArgs(
+                    "--add-opens=java.base/java.lang.reflect=ALL-UNNAMED",
+                    "--add-opens=java.base/java.lang=ALL-UNNAMED",
+                    "--add-opens=java.base/java.util=ALL-UNNAMED",
+                    "--add-opens=java.base/java.io=ALL-UNNAMED",
+                    "--add-opens=java.base/jdk.internal.access=ALL-UNNAMED",
+                    "-XX:+EnableDynamicAgentLoading"
+                )
+            }
         }
     }
     compileOptions {
